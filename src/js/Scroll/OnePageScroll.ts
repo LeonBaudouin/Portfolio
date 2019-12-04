5d462734283eacf15b36e4f0af3d0be44eb49028
LeonBaudouin <leondedouin@gmail.com>
2019-12-04 13:36:09 +0100

    Fixed fullscreen chrome mobile bug

--- src/js/Scroll/OnePageScroll.ts
+++ src/js/Scroll/OnePageScroll.ts
@@ -7,7 +7,6 @@ import { Point } from "../CustomTypes/Point";
 type ScrollCallback = (i: number) => void;
 
 export class OnePageScroll {
-
     bodyClassList: DOMTokenList = document.querySelector("body").classList;
     container: HTMLElement = document.querySelector(".container");
     currentSlideIndex: number = 0;
@@ -15,18 +14,23 @@ export class OnePageScroll {
 
     slideNumber: number;
     swipeHandler: SwipeHandler;
-    scrollAmount: number
+    scrollAmount: number;
     cooldown: number;
-    scrollCallbacks: ScrollCallback[]
+    scrollCallbacks: ScrollCallback[];
 
-    constructor(slideNumber: number, coolDown: number, scrollAmount: number, scrollCallbacks: ScrollCallback[] = []) {
+    constructor(
+        slideNumber: number,
+        coolDown: number,
+        scrollAmount: number,
+        scrollCallbacks: ScrollCallback[] = []
+    ) {
         this.cooldown = coolDown;
         this.scrollAmount = scrollAmount;
         this.slideNumber = slideNumber;
         this.scrollCallbacks = scrollCallbacks;
         this.bodyClassList.add("scroll-index-0");
 
-        this.InitEvent()
+        this.InitEvent();
     }
 
     private swipeCondition({ x, y }: Point): boolean {
@@ -37,8 +41,8 @@ export class OnePageScroll {
 
     private InitEvent(): void {
         this.swipeHandler = new SwipeHandler(
-            (p) => { this.DirectionScroll(p.y); },
-            (p) => this.swipeCondition(p)
+            p => this.DirectionScroll(p.y),
+            p => this.swipeCondition(p)
         );
 
         addWheelListener(this.container, (e: WheelEvent) => {
@@ -54,17 +58,21 @@ export class OnePageScroll {
             if (Math.abs(distance) > 80) {
                 this.DirectionScroll(distance);
             }
-        })
+        });
 
-        document.querySelectorAll('.sideButton').forEach(
-            el => el.addEventListener('touchmove',
-                e => e.preventDefault()
-            )
-        )
+        document
+            .querySelectorAll(".sideButton")
+            .forEach(el =>
+                el.addEventListener("touchmove", e => e.preventDefault())
+            );
 
-        this.container.addEventListener("touchmove", (e) => {
+        document
+            .querySelector(".loading-screen")
+            .addEventListener("touchmove", e => e.preventDefault());
+
+        this.container.addEventListener("touchmove", e => {
             e.preventDefault();
-        })
+        });
     }
 
     private DirectionScroll(direction: number) {
@@ -93,7 +101,7 @@ export class OnePageScroll {
         let currentScroll = this.scrollAmount * this.currentSlideIndex;
         let animationDistance = this.scrollAmount * destination - currentScroll;
 
-        this.scrollCallbacks.forEach(func => func(destination))
+        this.scrollCallbacks.forEach(func => func(destination));
         this.AnimationScroll(currentScroll, 0, animationDistance, 24);
 
         this.bodyClassList.remove("scroll-index-" + this.currentSlideIndex);
@@ -103,25 +111,32 @@ export class OnePageScroll {
 
     private Timeout(): void {
         this.isScrolling = true;
-        setTimeout(
-            (() => this.isScrolling = false),
-            this.cooldown
-        );
+        setTimeout(() => (this.isScrolling = false), this.cooldown);
     }
 
-
-    private AnimationScroll(currentScroll: number, currentTime: number, scrollDistance: number, animationDuration: number) {
-
+    private AnimationScroll(
+        currentScroll: number,
+        currentTime: number,
+        scrollDistance: number,
+        animationDuration: number
+    ) {
         if (animationDuration > currentTime) {
-
             currentTime++;
-            let factor = MathFunc.easeInOutQuad(currentTime / animationDuration);
+            let factor = MathFunc.easeInOutQuad(
+                currentTime / animationDuration
+            );
             window.scroll(0, currentScroll + scrollDistance * factor);
 
-            window.requestAnimationFrame(() => this.AnimationScroll(currentScroll, currentTime, scrollDistance, animationDuration));
+            window.requestAnimationFrame(() =>
+                this.AnimationScroll(
+                    currentScroll,
+                    currentTime,
+                    scrollDistance,
+                    animationDuration
+                )
+            );
         }
     }
-
 
     get CanScroll(): boolean {
         return !this.isScrolling;
