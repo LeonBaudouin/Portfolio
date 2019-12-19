2ee1fa2235d4f7c25a1bce204633620a73f722d8
LeonBaudouin <leondedouin@gmail.com>
2019-12-19 12:41:35 +0100

    Added fairy image and project ordering

--- Controller/LabList.php
+++ Controller/LabList.php
@@ -6,8 +6,8 @@ class LabList extends AbstractController
 {
     public static function execute($params)
     {
-        $projects = \Model\Project\ProjectRepository::getAll(['is_lab' => 1, 'is_visible' => 1]);
-        $projectsName = array_map(function($p) {return $p->getName();}, $projects);
+        $projects = \Model\Project\ProjectRepository::getAll(['is_lab' => 1, 'is_visible' => 1], true);
+        $projectsName = array_map(function ($p) {return $p->getName();}, $projects);
         self::getRenderer()->render('lab-list.twig', ['projects' => $projects, 'projectsName' => $projectsName]);
     }
 }
--- Controller/ProjectList.php
+++ Controller/ProjectList.php
@@ -6,8 +6,8 @@ class ProjectList extends AbstractController
 {
     public static function execute($params)
     {
-        $projects = \Model\Project\ProjectRepository::getAll(['is_lab' => 0, 'is_visible' => 1]);
-        $projectsName = array_map(function($p) {return $p->getName();}, $projects);
+        $projects = \Model\Project\ProjectRepository::getAll(['is_lab' => 0, 'is_visible' => 1], true);
+        $projectsName = array_map(function ($p) {return $p->getName();}, $projects);
         self::getRenderer()->render('project-list.twig', ['projects' => $projects, 'projectsName' => $projectsName]);
     }
 }
