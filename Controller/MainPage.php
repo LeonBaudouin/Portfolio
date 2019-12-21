39a7ffb4a890db2089d905a147ac91d776c7d82b
LeonBaudouin <leondedouin@gmail.com>
2019-12-21 18:21:02 +0100

    Added abyss to db

--- Controller/MainPage.php
+++ Controller/MainPage.php
@@ -6,8 +6,8 @@ class MainPage extends AbstractController
 {
     public static function execute($params)
     {
-        $labs = \Model\Project\ProjectRepository::getAll(['is_lab' => 1, 'is_featured' => 1], true);
-        $projects = \Model\Project\ProjectRepository::getAll(['is_lab' => 0, 'is_featured' => 1], true);
+        $labs = \Model\Project\ProjectRepository::getAll(['is_lab' => 1, 'is_featured' => 1, 'is_visible' => 1], true);
+        $projects = \Model\Project\ProjectRepository::getAll(['is_lab' => 0, 'is_featured' => 1, 'is_visible' => 1], true);
         self::getRenderer()->render('index.twig', ['projects' => $projects, 'labs' => $labs]);
     }
 }
